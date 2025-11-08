1daa063561a9bc7b32f4cb8ddce41fd1475ff96c
hElLo-WOrLd1402 <149694674+JOnHWeak@users.noreply.github.com>
2025-11-08 23:17:46 +0700

    fixx 3.144444

--- js/intro.js
+++ js/intro.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Chờ 3 giây để animation hoàn tất
     setTimeout(() => {
         // Chuyển hướng sang trang mindmap chính
-        window.location.href = 'index.html';
+        window.location.href = 'main.html';
     }, 3000); // 3 giây
 });
