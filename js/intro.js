f5ff0229d5f0e94dfca40c2538ccd1387f90f80b
hElLo-WOrLd1402 <149694674+JOnHWeak@users.noreply.github.com>
2025-11-08 22:47:39 +0700

    add intro

---
+++ js/intro.js
@@ -0,0 +1,8 @@
+// js/intro.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Chờ 3 giây để animation hoàn tất
+    setTimeout(() => {
+        // Chuyển hướng sang trang mindmap chính
+        window.location.href = 'index.html';
+    }, 3000); // 3 giây
+});
